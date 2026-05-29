dfd71246007426f1e6169830de9eb91ab5a1d755
gpakoh <gpakoh159@gmail.com>
2026-05-29 15:57:54 +0300

    test: cover security and CORS headers

--- app/main.py
+++ app/main.py
@@ -953,7 +953,7 @@ app.add_middleware(
     allow_origins=settings.cors_origins,
     allow_credentials=True,
     allow_methods=["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
-    allow_headers=["*"],
+    allow_headers=["Content-Type", "Authorization", "X-API-Key", "X-Request-ID"],
 )
 
 
